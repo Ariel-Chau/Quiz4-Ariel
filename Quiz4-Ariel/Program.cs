c959f03d38211d6f0e157ad718f26d13b973be94
Ariel Zhou <achauzho@my.centennialcollege.ca>
2024-03-28 14:12:39 -0400

    first commit

--- Quiz4-Ariel/Program.cs
+++ Quiz4-Ariel/Program.cs
@@ -9,7 +9,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts..
     app.UseHsts();
 }
 
@@ -23,3 +23,4 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
